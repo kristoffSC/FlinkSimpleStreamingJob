386f7284e763fad6c0a1781ed3fedc63bf90a859
Krzysztof Chmielewski <krzysiek.chmielewski@gmail.com>
2023-09-07 09:25:36 +0200

    using CheckpointedFunction on keyed Stream

--- src/main/java/org/example/KeyCounter.java
+++ src/main/java/org/example/KeyCounter.java
@@ -3,53 +3,44 @@ package org.example;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.runtime.state.FunctionInitializationContext;
-import org.apache.flink.runtime.state.FunctionSnapshotContext;
-import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class KeyCounter extends KeyedProcessFunction<Integer, Integer, Tuple2<Integer, Integer>>
-    implements CheckpointedFunction {
+public class KeyCounter extends KeyedProcessFunction<Integer, Integer, Tuple2<Integer, Integer>> {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(KeyCounter.class);
 
-  private int keyCounter;
+  private ValueState<Integer> keyCounterState;
 
-  private transient ValueState<Integer> keyCounterState;
+  @Override
+  public void open(Configuration parameters) throws Exception {
+    super.open(parameters);
 
-  public KeyCounter() {}
+    LOGGER.info("Key counter state init.");
+    keyCounterState = getRuntimeContext()
+        .getState(new ValueStateDescriptor<>("keyCounter", Integer.class));
+
+  }
 
   @Override
   public void processElement(final Integer value,
                              final Context context,
                              final Collector<Tuple2<Integer, Integer>> collector) throws Exception {
 
-    keyCounter = keyCounterState.value();
+
+    int keyCounter;
+    Integer valueFromState = keyCounterState.value();
+    if (valueFromState == null) {
+      keyCounterState.update(0);
+      keyCounter = 0;
+    } else {
+      keyCounter = valueFromState;
+    }
 
     collector.collect(Tuple2.of(context.getCurrentKey(), ++keyCounter));
-
-  }
-
-  @Override
-  public void initializeState(final FunctionInitializationContext context) throws Exception {
-
-    LOGGER.info("Key counter state init.");
-    keyCounterState = getRuntimeContext()
-        .getState(new ValueStateDescriptor<>("keyCounter", Integer.class));
-
-    if (keyCounterState != null) {
-      keyCounter = keyCounterState.value();
-    }
-  }
-
-  @Override
-  public void snapshotState(final FunctionSnapshotContext context) throws Exception {
-    LOGGER.info("Key counter state snapshot.");
-    if (keyCounterState != null) {
-      keyCounterState.update(keyCounter);
-    }
+    keyCounterState.update(keyCounter);
   }
 }
